d90c83061651b59caf225f59416b507cebc25deb
DESKTOP-R1NAFC1\Kopche <pukarcho@abv.bg>
2023-04-19 16:25:40 +0300

    https fix

--- src/client/weatherClient.js
+++ src/client/weatherClient.js
@@ -13,7 +13,7 @@ export const getCurrentWeather = async (city) => {
 };
 
 export const getCurrentAirPollution = async (coord) => {
-  return await axios.get(`http://api.openweathermap.org/data/2.5/air_pollution?lat=${coord[0]}&lon=${coord[1]}&appid=0f4e90f3fa8b0fa05b09b7551b747ec5`)
+  return await axios.get(`https://api.openweathermap.org/data/2.5/air_pollution?lat=${coord[0]}&lon=${coord[1]}&appid=0f4e90f3fa8b0fa05b09b7551b747ec5`)
     .then((response) => {
       return response.data;
     })
